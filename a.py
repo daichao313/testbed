fb99600eca807c3e360d026fe1703293345ba4b8
daichao <daichao1989@gmail.com>
2016-04-21 19:50:25 +0800

    1

---
+++ a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+# Author: daichao
+# Created Time: 2016-04-21 19:27:40
+# Last Modified: 2016-04-21 19:27:49
+# File Name: a.py
+# Description:
+
+import time
